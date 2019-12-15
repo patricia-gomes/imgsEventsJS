7f2c34381eec1e9b78d2104cef960b8fcdac0149
Patricia Gomes <patriciagomesro33@gmail.com>
2019-12-15 16:19:34 -0400

    html, css e eventos javascript

---
+++ README.md
@@ -0,0 +1,4 @@
+# ImgsEventsJS
+
+Um exemplo simples de como usar alguns eventos javascript em imagens.Trocar de imagem quando passar o mouse em cima ou quando tirar o mouse de cima e altera a imagem quando você clica no botão.
+Os eventos utilizados nesse exemplo são: onclick, onmouseover, onmouseout.
---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+/*
+As 3 funções selecionam o id da imagem principal e subistitui a imagem de
+acordo com a função chamada.O que muda nas 3 funções são o caminho das 
+imagens.
+*/
+function imgCat() {
+	document.getElementById('imgPrincipal').src= "assets/imgs/cat.jpg";
+}
+function imgLion() {
+	document.getElementById('imgPrincipal').src = "assets/imgs/lion.jpg";
+}
+function imgDogs() {
+	document.getElementById('imgPrincipal').src = "assets/imgs/dogs.jpg";
+}
